9e69abc92af6024725e3ddff49492c129a04dc81
Guilherme Maia <141831938+Guilhlm@users.noreply.github.com>
2024-06-21 22:43:00 -0300

    funfou

--- vagner-carrinho/script.js
+++ vagner-carrinho/script.js
@@ -2,68 +2,62 @@ let carrinho = [];
 let total = 0;
 
 function addCarrinho(produto, valor) {
-
     let fim = false;
 
-    for (let i = 0; i < carrinho.length; i++) { //leght eh o ultimo itemn 
-
+    for (let i = 0; i < carrinho.length; i++) {
         if (carrinho[i].produto === produto) {
-
             carrinho[i].qtd++;
-            fim = true; 
+            fim = true;
             break;
         }
     }
 
     if (!fim) {
-
-        carrinho.push({ produto, valor, qtd: 1 }); // push eh puxar a info
+        carrinho.push({ produto, valor, qtd: 1 });
     }
 
     atualizaCarrinho();
 }
 
 function atualizaCarrinho() {
-
     const cartItens = document.getElementById('carrinho-itens');
     cartItens.innerHTML = '';
     total = 0;
 
-    carrinho.forEach((item, index) => {
-
+    carrinho.forEach((item) => {
         total += item.valor * item.qtd;
-        cartItens.innerHTML += `
+
+        const productQuantitySpan = document.querySelector(`.product-quantity[data-product="${item.produto}"]`);
+        if (productQuantitySpan) {
+            productQuantitySpan.innerText = `${item.qtd}x`;
+        }      
         
-        <div class="carrinho-itens">
+    });
 
-            <span>${item.produto} (${item.qtd} x) </span>
-            <span>R$ ${item.valor * item.qtd}</span>
-            <button onClick="removeItemCarrinho(${index})"> Remove Compre </button>
-
-        </div>
-           
-        `;
-    })
-
-    document.getElementById('carrinho-total').innerText = total;
+    document.getElementById('carrinho-total').innerText = total.toFixed(2);
 }
 
-function removeItemCarrinho(index){
-
-    total -= carrinho[index].valor * carrinho[index].qtd;
-    carrinho.splice(index,1);
+function removeItemCarrinho(produto) {
+    for (let i = 0; i < carrinho.length; i++) {
+        if (carrinho[i].produto === produto) {
+            if (carrinho[i].qtd > 1) {
+                carrinho[i].qtd--;
+            } else {
+                carrinho.splice(i, 1);
+            }
+            break;
+        }
+    }
     atualizaCarrinho();
 }
 
-function fecharPedido(){
-
-    if(carrinho.length === 0){
-
-        alert(`Seu carrinho  está vazio`);  
+function fecharPedido() {
+    if (carrinho.length === 0) {
+        alert('Seu carrinho está vazio');
         return;
     }
 
-    alert(`Compra finalizada com sucesso: R$ ${total}`);
+    alert(`Compra finalizada com sucesso: R$ ${total.toFixed(2)}`);
     carrinho = [];
     atualizaCarrinho();
 }
